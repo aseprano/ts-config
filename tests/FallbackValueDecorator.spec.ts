89318d40ff1912ce6202260f15eec2b5de072f5b
Antonio Seprano <antonio.seprano@docebo.com>
2019-12-09 23:24:15 +0100

    Better testing of FallbackValueDecorator

--- tests/FallbackValueDecorator.spec.ts
+++ tests/FallbackValueDecorator.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryConfig } from "../src/impl/InMemoryConfig";
 import { FallbackValueDecorator } from "../src/impl/FallbackValueDecorator";
+import { mock, when, instance } from "ts-mockito";
 
 describe('FallbackValueDecorator', () => {
     const fakeConfig = new InMemoryConfig();
@@ -7,6 +8,14 @@ describe('FallbackValueDecorator', () => {
 
     const fallback = new FallbackValueDecorator(fakeConfig);
     
+    it('provides the default_value to the inner config', async () => {
+        const mockedConfig = mock(InMemoryConfig);
+        when(mockedConfig.get('foo', 10)).thenResolve(12);
+
+        const fallback = new FallbackValueDecorator(instance(mockedConfig));
+        expect(await fallback.get('foo', 10)).toEqual(12);
+    });
+
     it('returns the inner value if found', async () => {
         expect(await fallback.get('foo', 10)).toEqual('bar');
     });
